740cdcaac9b1d9fd0314a02f643c77f820ad22dd
pavel-ryzhov <88110675+pavel-ryzhov@users.noreply.github.com>
2021-08-04 19:28:00 +0300

    Update N1.java

--- src/N1.java
+++ src/N1.java
@@ -1,9 +1,11 @@
 public class N1 {
     public static void main(String[] args) {
         double a = 10;
-        for (int i = 1; i < 7; i++) {
+        double b = 0;
+        for (int i = 0; i < 7; i++) {
+            b += a;
             a += a * 0.1;
         }
-        System.out.println(a);
+        System.out.println(b);
     }
 }
